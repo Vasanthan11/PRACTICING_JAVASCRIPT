2db3b03acfa9f323141e1a9d8768709b271ab388
Vasanthan11 <vasavj70@gmail.com>
2024-07-15 03:08:57 +0530

    Practicing Javascript File Two

--- Practice.js
+++ Practice.js
@@ -70,3 +70,37 @@ function transform(number, func){
 
 let result = transform(10, double);
 console.log(result);
+
+
+
+/*
+Exercise: Arrays
+For this exercise, your task is to push two new numbers onto the existing numbers array: 8 and 2, in that order.
+Don't create a new array, don't overwrite the existing array - instead manipulate the existing array as taught earlier in the course.
+ */
+let numbers = [10, 3, -1];
+function solve() {
+    // Ensure the array length is exactly 3 before pushing new elements
+    numbers.length = 3;
+    numbers.push(8);
+    numbers.push(2);
+}
+solve();
+console.log(numbers); 
+
+
+
+/*
+Exercise: Objects
+For this exercise, your task is to create a new JavaScript object that should be stored in the courseData variable.
+The to-be-created object must have three properties:
+title => a string (must be "JavaScript - The Complete Guide")
+duration => a positive number of your choice (e.g., 30)
+levels => an array with exactly two elements (must be "Beginner" and "Advanced")
+You don't have to do anything else with the object - just create it and make sure the data mentioned above is stored correctly. */
+let courseData = {
+    title: "JavaScript - The Complete Guide",
+    duration: 30,
+    levels: ["Beginner", "Advanced"]
+};
+console.log(courseData);
