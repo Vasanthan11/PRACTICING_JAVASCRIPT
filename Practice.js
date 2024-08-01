90d82b7bdb6af6942e79438ed006125f490469d1
Vasanthan11 <vasavj70@gmail.com>
2024-08-01 05:45:49 +0530

    Javascript Practicing Day 6

--- Practice.js
+++ Practice.js
@@ -122,3 +122,25 @@ for(let i=0; i<100; i++){
 
 
 
+
+
+/*For this exercise, you should add some elements to the DOM via JavaScript.
+To be precise, you must add two "course goal" items => two <li> items to the <ul> that already exists on the page:
+The first course goal <li> that's added via JavaScript should contain the text "Learn JavaScript" (without quotes)
+The second goal <li> item should contain the text "Master JavaScript" (also without quotes)
+Again: You must do this via JavaScript, NOT by editing the HTML code!
+Important: Put all your DOM editing code into the pre-defined solve() function. */
+
+             const ul = document.querySelector("ul");
+            function solve() {
+                const firstLi = document.createElement("li");
+                firstLi.innerText = "Learn JavaScript";
+                ul.appendChild(firstLi);
+
+                const secondLi = document.createElement("li");
+                secondLi.innerText = "Master JavaScript";
+                ul.appendChild(secondLi);
+            }
+            
+            solve();
+
